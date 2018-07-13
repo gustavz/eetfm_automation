37fe692b82e3fb89d7e990fc840f344b196d70a2
GustavZ <gustav.zitzewitz@gmx.de>
2018-07-13 11:15:07 +0200

    typo in timliner prepare

--- eetfm_automation.py
+++ eetfm_automation.py
@@ -11,6 +11,7 @@ import datetime
 import subprocess
 import numpy as np
 import tensorflow as tf
+import re
 
 def get_models_list(models_dir):
     """
@@ -81,7 +82,7 @@ def export_model(base_model_name,
             --output_directory={export_model_path} \
             --config_override={config_override}'.format(**locals())
 
-    print ("> Exporting model {export_model_name}".format(**locals()))
+    print ("> Start exporting model {export_model_name}".format(**locals()))
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     process.wait()
     print process.returncode
@@ -105,7 +106,7 @@ def evaluate_model(model_name,models_dir,tf_dir):
             --eval_dir={model_path}/eval \
             --run_once=True'.format(**locals())
 
-    print ("> Evaluating model {model_name}".format(**locals()))
+    print ("> Start evaluating model {model_name}".format(**locals()))
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     process.wait()
     print process.returncode
@@ -120,6 +121,56 @@ def log_evaldate(model_path):
     file = open('{model_path}/eval_log.txt'.format(**locals()),mode='a')
     file.write("\nModel evaluated at: {eval_date}".format(**locals()))
     file.close()
+
+def benchmark_model(model_name,model_dir,tf_dir,shape):
+    root_dir = os.getcwd()
+    in_graph = "{root_dir}/{model_dir}/{model_name}/frozen_inference_graph.pb".format(**locals())
+    inputs = 'image_tensor'
+    input_type = 'uint8'
+    outputs = 'num_detections,detection_boxes,detection_scores,detection_classes,detection_masks'
+    log_file = '{root_dir}/{model_dir}/benchmark_{model_name}.txt'.format(**locals())
+
+    cmd = 'cd {tf_dir}\
+    bazel run tensorflow/tools/benchmark:benchmark_model -- \
+     --graph={in_graph} \
+     --input_layer={inputs} \
+     --input_layer_type={input_type} \
+     --input_layer_shape={shape} \
+     --output_layer={outputs} \
+     --show_run_order=true \
+     --show_time=true \
+     --show_memory=true \
+     --show_summary=true \
+     --show_flops=true \
+     2>&1 | tee {log_file}'. format(**locals())
+    print ("> Benchmarking model {model_name}".format(**locals()))
+    process = subprocess.Popen('/bin/bash', stdout=subprocess.PIPE)
+    out, err = process.communicate(cmd)
+    print out
+    process.wait()
+    print process.returncode
+    if process.returncode == 0:
+        print("> Benchmark of model {model_name} complete".format(**locals()))
+    else:
+        print("> ERROR: Benchmark of model {model_name} NOT complete".format(**locals()))
+        os.remove(log_file)
+
+
+def get_model_shape(model_path):
+    """
+    get input dimensions of model
+    """
+
+    f = open(model_path+"/pipeline.config")
+    for i, line in enumerate(f):
+        if i == 6:
+            min = re.search(r'\d+', line).group()
+        if i == 7:
+            max = re.search(r'\d+', line).group()
+            break
+    f.close()
+    shape = "1,{min},{max},3".format(**locals())
+    return shape
 
 
 def main():
@@ -145,17 +196,25 @@ def main():
     NUM_EXAMPLES = cfg['NUM_EXAMPLES']
     METRICS = cfg['METRICS']
     SKIP_EVALED = cfg['SKIP_EVALED']
+    TF_DIR = cfg['TF_DIR']
+    BENCHMARK_MODELS_LIST = cfg['BENCHMARK_MODELS_LIST']
 
+
+    """
+    MODEL EXPORTATION
+    """
     # load all models from base models directory if no specific list is given
     if not EXPORT_MODELS_LIST:
         EXPORT_MODELS_LIST = get_models_list(BASE_MODELS_DIR)
-
-    ALREADY_EXPORTED_MODELS_LIST = get_models_list(EXPORT_MODELS_DIR)
-
     # export models if not set to False
     if not EXPORT_MODELS_LIST[0] is False:
-        print("> Start Exporting models")
+        # count models already exported
+        already_exported_models_list = get_models_list(EXPORT_MODELS_DIR)
+        num_models_to_export = len(PROPOSALS_LIST)*len(FS_SCORES_LIST)*len(FS_IOUS_LIST)*len(SS_SCORES_LIST)*len(SS_IOUS_LIST)
+
+        print("> Start Exporting {num_models_to_export} models".format(**locals()))
         # crazy exportation loop is crazy
+        i = 0
         for base_model_name in EXPORT_MODELS_LIST:
             for proposals in PROPOSALS_LIST:
                 for fs_score in FS_SCORES_LIST:
@@ -164,30 +223,82 @@ def main():
                             for ss_iou in SS_IOUS_LIST:
                                 suffix = "_{proposals}p_{fs_score}fs_{fs_iou}fiou_{ss_score}ss_{ss_iou}siou".format(**locals())
                                 export_model_name = base_model_name +  suffix
-                                if not (ALREADY_EXPORTED_MODELS_LIST and export_model_name in ALREADY_EXPORTED_MODELS_LIST):
+                                # check if model is already exported
+                                i += 1
+                                print("> {}/{}".format(i,num_models_to_export))
+                                if export_model_name in already_exported_models_list:
+                                    print("> Skipping Export: {export_model_name} already exported".format(**locals()))
+                                else:
                                     config_override = create_config_override(fs_score,fs_iou,ss_score,ss_iou,proposals,NUM_EXAMPLES,METRICS)
                                     export_model(base_model_name,export_model_name,config_override,BASE_MODELS_DIR,EXPORT_MODELS_DIR,TF_ODAPI_DIR)
-                                else:
-                                    print("> Skipping: Model {export_model_name} already exported".format(**locals()))
     else:
-        print("> Skipping: user request")
+        print("> Skipping Export: User Request")
 
+    """
+    MODEL EVALUATION
+    """
     # load all models from export models directory if no specific list is given
     if not EVAL_MODELS_LIST:
+        eval_all = True
         EVAL_MODELS_LIST = get_models_list(EXPORT_MODELS_DIR)
-
     # evaluate models if not set to False
     if not EVAL_MODELS_LIST[0] is False:
-        print("> Start Evaluating models")
+
+        # count models already evaluated
+        already_evaled_models_list = []
         for model_name in EVAL_MODELS_LIST:
             model_path = EXPORT_MODELS_DIR+"/"+model_name
-            # check if model was already evaluated
-            if os.path.exists(model_path+"/eval_log.txt") and SKIP_EVALED:
-                print("> Skipping: Model {model_name} already evaluated".format(**locals()))
+            if os.path.exists(model_path+"/eval_log.txt"):
+                already_evaled_models_list.append(model_name)
+        if eval_all:
+            num_models_to_eval = len(EVAL_MODELS_LIST) - len(already_evaled_models_list)
+        else:
+            num_models_to_eval = len(EVAL_MODELS_LIST)
+
+        print("> Start Evaluating {num_models_to_eval} models".format(**locals()))
+        for i,model_name in enumerate(EVAL_MODELS_LIST):
+            # check if model is already evaluated
+            print("> {}/{}".format(i+1,num_models_to_eval))
+            if (model_name in already_evaled_models_list) and SKIP_EVALED:
+                print("> Skipping Evaluation: {model_name} already evaluated".format(**locals()))
             else:
                 evaluate_model(model_name,EXPORT_MODELS_DIR,TF_ODAPI_DIR)
     else:
-        print("> Skipping: user request")
+        print("> Skipping Evaluation: User Request")
+
+    """
+    MODEL BENCHMARK TESTING
+    """
+    # load all models from export models directory if no specific list is given
+    if not BENCHMARK_MODELS_LIST:
+        benchmark_all = True
+        BENCHMARK_MODELS_LIST = get_models_list(EXPORT_MODELS_DIR)
+    if not BENCHMARK_MODELS_LIST[0] is False:
+        # count models already evaluated
+        already_benchmarked_models_list = []
+        for model_name in BENCHMARK_MODELS_LIST:
+            model_path = EXPORT_MODELS_DIR+"/"+model_name
+            if os.path.exists("{model_path}/benchmark_{model_name}.txt".format(**locals())):
+                already_benchmarked_models_list.append(model_name)
+        if benchmark_all:
+            num_models_to_benchmark = len(BENCHMARK_MODELS_LIST) - len(already_benchmarked_models_list)
+        else:
+            num_models_to_benchmark = len(BENCHMARK_MODELS_LIST)
+
+        print("> Start Benchmarking {num_models_to_benchmark} models".format(**locals()))
+        for i,model_name in enumerate(BENCHMARK_MODELS_LIST):
+            # check if model is already evaluated
+            print("> {}/{}".format(i+1,num_models_to_benchmark))
+            if model_name in already_benchmarked_models_list:
+                print("> Skipping Benchmark test: {model_name} already benchmarked".format(**locals()))
+            else:
+                model_path = EXPORT_MODELS_DIR+"/"+model_name
+                shape = get_model_shape(model_path)
+                benchmark_model(model_name,EXPORT_MODELS_DIR,TF_DIR,shape)
+    else:
+        print("> Skipping Evaluation: User Request")
+
+
 
     print("> eetfm_automation complete")
 
